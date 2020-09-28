00fa9a58b601b589c04a24340742447b012ddc57
sgsettle <s.settle41@gmail.com>
2020-09-28 10:51:48 -0400

    Add project files.

---
+++ Glamboyant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Glamboyant.MvcApplication" Language="C#" %>
---
+++ Glamboyant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Glamboyant.Startup))]
+namespace Glamboyant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
